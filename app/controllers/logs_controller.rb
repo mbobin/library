19225f3839eecf04ce1a7093a6caabc35a205043
Marius Bobin <marius.bobin@devacademy.ro>
2018-03-14 22:52:45 +0200

    filter logs

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,5 +1,8 @@
 class LogsController < ApplicationController
   def index
-    @logs = ImportLog.order(created_at: :desc).page(params[:page]).per(50)
+    @logs = ImportLog.order(created_at: :desc)
+    @logs = @logs.with_error if params.key?(:error)
+    @logs = @logs.halted if params.key?(:halted)
+    @logs = @logs.page(params[:page]).per(50)
   end
 end
--- app/models/import_log.rb
+++ app/models/import_log.rb
@@ -1,5 +1,8 @@
 class ImportLog < ApplicationRecord
+  scope :with_error, -> { where("data->>'error' IS NOT NULL") }
+  scope :halted, -> { where("data->>'halted' IS NOT NULL") }
+
   def fail?
-    data["error"] || !(data["author_ids"] && data["book_id"])
+    data["error"] || data["halted"]
   end
 end
