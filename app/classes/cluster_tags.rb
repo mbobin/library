ff4df4518ad01ad2687fbc6be85b82b202483753
Marius Bobin <marius@mbobin.me>
2018-11-15 15:48:41 +0200

    add cluster tags

---
+++ app/classes/cluster_tags.rb
@@ -0,0 +1,56 @@
+ass ClusterTags
+  CATEGORIES = {
+    "xxl" => 5_000..1_000_000,
+    "xl"  => 1_000..5_000,
+    "lg"  => 500..1_000,
+    "md"  => 100..500,
+    "sm"  => 50..100,
+  }.freeze
+
+ INVERTED_CATEGORIES = CATEGORIES.invert.freeze
+
+  class << self
+    def call
+      new.call
+    end
+
+    def each_category(&block)
+      CATEGORIES.keys.each(&block)
+    end
+  end
+
+  def call
+    cluster_tags
+  end
+
+  private
+
+  def cluster
+    @cluster ||= ApplicationRecord.connection.execute(cluster_sql).to_a
+  end
+
+  def cluster_tags
+    cluster
+      .group_by { |tag| category(tag["frequency"]) }
+      .transform_values { |tags| tags.sort_by { |tag| -tag["frequency"] } }
+  end
+
+  def category(number)
+    INVERTED_CATEGORIES
+      .detect { |(range, value)| range.cover?(number.to_i) }
+      .to_a
+      .last || "xs"
+  end
+
+  def cluster_sql
+    <<~SQL
+      select word, count(word) as frequency
+      from(
+        select(unnest(tags)) as word from books
+      ) words
+      group by word
+      order by frequency desc
+    SQL
+  end
+end
+
--- app/controllers/library_controller.rb
+++ app/controllers/library_controller.rb
@@ -1,8 +1,6 @@
 class LibraryController < ApplicationController
   def index
-    @cluster = ApplicationRecord.connection.execute("select word, count(word) \
-       as frequency from(select(unnest(tags)) as word from books) words group \
-        by word order by 2 desc"
-      ).to_a
+    @cluster = ClusterTags.call
   end
 end
+
