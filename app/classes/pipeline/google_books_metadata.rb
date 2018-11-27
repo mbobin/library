f636609f8dd6f7389135e13e6e69518bf0c35b60
Marius Bobin <marius@mbobin.me>
2018-11-27 21:36:27 +0200

    add missing fields to null object

--- app/classes/pipeline/google_books_metadata.rb
+++ app/classes/pipeline/google_books_metadata.rb
@@ -1,6 +1,10 @@
 module Pipeline
   class GoogleBooksMetadata < Base
-    NoBook = Struct.new(:isbn)
+    NoBook = Struct.new(:isbn, :description, :published_date) do
+      def authors
+        @authors ||= []
+      end
+    end
 
     private
     attr_reader :book_data
